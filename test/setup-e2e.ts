561fa2062402f01926029d49ab091538b4d97817
jeanmbiz <jeanmbiz@hotmail.com>
2024-10-21 09:57:05 -0300

    feat: add setup to tests e2e

---
+++ test/setup-e2e.ts
@@ -0,0 +1,44 @@
+import 'dotenv/config'
+
+import { PrismaClient } from '@prisma/client'
+import { randomUUID } from 'node:crypto'
+import { execSync } from 'node:child_process'
+// import { DomainEvents } from '@/core/events/domain-events'
+// import { Redis } from 'ioredis'
+// import { envSchema } from '@/infra/env/env'
+
+// config({ path: '.env', override: true })
+// config({ path: '.env.test', override: true })
+
+const prisma = new PrismaClient()
+
+function generateUniqueDatabaseURL(schemaId: string) {
+  if (!process.env.DATABASE_URL) {
+    throw new Error('Please provider a DATABASE_URL environment variable')
+  }
+
+  const url = new URL(process.env.DATABASE_URL)
+
+  url.searchParams.set('schema', schemaId)
+
+  return url.toString()
+}
+
+const schemaId = randomUUID()
+
+beforeAll(async () => {
+  const databaseURL = generateUniqueDatabaseURL(schemaId)
+
+  process.env.DATABASE_URL = databaseURL
+
+  // DomainEvents.shouldRun = false
+
+  // await redis.flushdb()
+
+  execSync('npx prisma migrate deploy')
+})
+
+afterAll(async () => {
+  await prisma.$executeRawUnsafe(`DROP SCHEMA IF EXISTS "${schemaId}" CASCADE`)
+  await prisma.$disconnect()
+})
