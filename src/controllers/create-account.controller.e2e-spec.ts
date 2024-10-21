684f4c8426184daf6c789b55bd9bf3065b5d2816
jeanmbiz <jeanmbiz@hotmail.com>
2024-10-21 10:44:38 -0300

    feat: add create account controller e2e test

---
+++ src/controllers/create-account.controller.e2e-spec.ts
@@ -0,0 +1,42 @@
+import { AppModule } from '@/app.module'
+import { Test } from '@nestjs/testing'
+import { INestApplication } from '@nestjs/common'
+import request from 'supertest'
+import { PrismaService } from '@/prisma/prisma.service'
+
+describe('Create Account (E2E)', () => {
+  let app: INestApplication
+  let prisma: PrismaService
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile()
+
+    app = moduleRef.createNestApplication()
+
+    prisma = moduleRef.get(PrismaService)
+
+    await app.init()
+  })
+
+  test('[POST] /accounts', async () => {
+    // faz requisição para a rota
+    const response = await request(app.getHttpServer()).post('/accounts').send({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    })
+
+    expect(response.statusCode).toBe(201)
+
+    // busca usuário no banco de dados
+    const userOnDatabase = await prisma.user.findUnique({
+      where: {
+        email: 'johndoe@example.com',
+      },
+    })
+
+    expect(userOnDatabase).toBeTruthy()
+  })
+})
