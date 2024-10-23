97538369de70720c0fb6b1d6acb794817ecd8211
jeanmbiz <jeanmbiz@hotmail.com>
2024-10-23 11:30:50 -0300

    feat: add QuestionsRepository, PrismaQuestionsRepository in DatabaseModule

--- src/infra/database/database.module.ts
+++ src/infra/database/database.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common'
 import { PrismaService } from './prisma/prisma.service'
+import { PrismaQuestionsRepository } from './prisma/repositories/prisma-questions-repository'
+import { QuestionsRepository } from '@/domain/forum/application/repositories/questions-repository'
 
 @Module({
   // provider: para PrismaService ficar disponível dentro deste módulo
-  providers: [PrismaService],
+  providers: [
+    PrismaService,
+    {
+      // provide: nome classe abstrata QuestionsRepository
+      provide: QuestionsRepository,
+      // utilize PrismaQuestionsRepository
+      useClass: PrismaQuestionsRepository,
+    },
+  ],
   // exports: para PrismaService ser utilizado em todos os módulos que importarem DatabaseModule
-  exports: [PrismaService],
+  exports: [
+    PrismaService,
+    // exporta classe abstrata
+    QuestionsRepository,
+  ],
 })
 export class DatabaseModule {}
