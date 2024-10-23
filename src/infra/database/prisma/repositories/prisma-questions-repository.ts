7550645de8c88369b3434a6ce6061cc60d879df3
jeanmbiz <jeanmbiz@hotmail.com>
2024-10-23 10:10:46 -0300

    feat: add prisma questions repository

---
+++ src/infra/database/prisma/repositories/prisma-questions-repository.ts
@@ -0,0 +1,86 @@
+import { PaginationParams } from '@/core/repositories/pagination-params'
+import { QuestionsRepository } from '@/domain/forum/application/repositories/questions-repository'
+import { Question } from '@/domain/forum/enterprise/entities/question'
+import { Injectable } from '@nestjs/common'
+import { PrismaService } from '../prisma.service'
+import { PrismaQuestionMapper } from '../mappers/prisma-question-mapper'
+
+// Injectable: Este repositório será injetado nos casos de uso
+@Injectable()
+// implementar Repositório(contrato) da camada de domínio
+export class PrismaQuestionsRepository implements QuestionsRepository {
+  // injeta dependencia do Prisma
+  constructor(private prisma: PrismaService) {}
+
+  async create(question: Question): Promise<void> {
+    const data = PrismaQuestionMapper.toPrisma(question)
+
+    await this.prisma.question.create({
+      data,
+    })
+  }
+
+  async save(question: Question): Promise<void> {
+    const data = PrismaQuestionMapper.toPrisma(question)
+
+    await this.prisma.question.update({
+      where: {
+        id: data.id,
+      },
+      data,
+    })
+  }
+
+  async findById(questionId: string): Promise<Question | null> {
+    const question = await this.prisma.question.findUnique({
+      where: {
+        id: questionId,
+      },
+    })
+
+    if (!question) {
+      return null
+    }
+
+    // mapper: faz conversão da Question (representação da tabela Question no DB) para entidade de Domínio
+    return PrismaQuestionMapper.toDomain(question)
+  }
+
+  async findBySlug(slug: string): Promise<Question | null> {
+    const question = await this.prisma.question.findUnique({
+      where: {
+        slug,
+      },
+    })
+
+    if (!question) {
+      return null
+    }
+
+    // mapper: faz conversão da Question (representação da tabela Question no DB) para entidade de Domínio
+    return PrismaQuestionMapper.toDomain(question)
+  }
+
+  async findManyRecent({ page }: PaginationParams): Promise<Question[]> {
+    const questions = await this.prisma.question.findMany({
+      orderBy: {
+        createdAt: 'desc',
+      },
+      take: 20,
+      skip: (page - 1) * 20,
+    })
+
+    // mapper: faz conversão da Question (representação da tabela Question no DB) para entidade de Domínio
+    return questions.map(PrismaQuestionMapper.toDomain)
+  }
+
+  async delete(question: Question): Promise<void> {
+    const data = PrismaQuestionMapper.toPrisma(question)
+
+    await this.prisma.question.delete({
+      where: {
+        id: data.id,
+      },
+    })
+  }
+}
