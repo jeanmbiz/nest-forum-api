f821dea732f4ee3bae6b1d2bb9a743526077f342
jeanmbiz <jeanmbiz@hotmail.com>
2024-11-02 16:33:41 -0300

    feat: add StudentsRepository and PrismaStudentsRepository in DatabaseModule

--- src/infra/database/database.module.ts
+++ src/infra/database/database.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common'
 import { PrismaService } from './prisma/prisma.service'
 import { PrismaQuestionsRepository } from './prisma/repositories/prisma-questions-repository'
 import { QuestionsRepository } from '@/domain/forum/application/repositories/questions-repository'
+import { StudentsRepository } from '@/domain/forum/application/repositories/students-repository'
+import { PrismaStudentsRepository } from './prisma/repositories/prisma-students-repository'
 
 @Module({
   // provider: para PrismaService ficar disponível dentro deste módulo
@@ -13,12 +15,18 @@ import { QuestionsRepository } from '@/domain/forum/application/repositories/que
       // utilize PrismaQuestionsRepository
       useClass: PrismaQuestionsRepository,
     },
+    {
+      // provide: nome classe abstrata
+      provide: StudentsRepository,
+      useClass: PrismaStudentsRepository,
+    },
   ],
   // exports: para PrismaService ser utilizado em todos os módulos que importarem DatabaseModule
   exports: [
     PrismaService,
     // exporta classe abstrata
     QuestionsRepository,
+    StudentsRepository,
   ],
 })
 export class DatabaseModule {}
