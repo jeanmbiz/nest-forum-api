3f36258995ce648aad0e3c2cadebf7b41fb09ee9
jeanmbiz <jeanmbiz@hotmail.com>
2024-11-02 11:09:47 -0300

    feat: add email and password in StudentProps

--- src/domain/forum/enterprise/entities/student.ts
+++ src/domain/forum/enterprise/entities/student.ts
@@ -3,8 +3,22 @@ import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 
 export interface StudentProps {
   name: string
+  email: string
+  password: string
 }
 export class Student extends Entity<StudentProps> {
+  get name() {
+    return this.props.name
+  }
+
+  get email() {
+    return this.props.email
+  }
+
+  get passowrd() {
+    return this.props.password
+  }
+
   // abstrair a criação das entidades
   static create(props: StudentProps, id?: UniqueEntityID) {
     const student = new Student(props, id)
