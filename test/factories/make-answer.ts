ef29a8834a8ddcd4a875910b796ee7ede2f625d5
jeanmbiz <jeanmbiz@hotmail.com>
2024-11-06 15:01:13 -0300

    feat: add makePrisma methods in factories

--- test/factories/make-answer.ts
+++ test/factories/make-answer.ts
@@ -1,7 +1,10 @@
+import { PrismaAnswerMapper } from './../../src/infra/database/prisma/mappers/prisma-answer-mapper'
 import { AnswerProps } from '../../src/domain/forum/enterprise/entities/answer'
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import { Answer } from '@/domain/forum/enterprise/entities/answer'
+import { PrismaService } from '@/infra/database/prisma/prisma.service'
 import { faker } from '@faker-js/faker'
+import { Injectable } from '@nestjs/common'
 
 export function makeAnswer(
   // para criar answers fakes
@@ -21,4 +24,19 @@ export function makeAnswer(
   )
 
   return answer
+}
+
+@Injectable()
+export class AnswerFactory {
+  constructor(private prisma: PrismaService) {}
+
+  async makePrismaAnswer(data: Partial<AnswerProps> = {}): Promise<Answer> {
+    const answer = makeAnswer(data)
+
+    await this.prisma.answer.create({
+      data: PrismaAnswerMapper.toPrisma(answer),
+    })
+
+    return answer
+  }
 }
