1de2be95f99aae0884e47221fb73f5d2fe234f43
jeanmbiz <jeanmbiz@hotmail.com>
2024-11-11 17:18:47 -0300

    feat: add DeleteQuestionController and DeleteQuestionUseCase

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -13,6 +13,8 @@ import { GetQuestionBySlugController } from './controllers/get-question-by-slug.
 import { GetQuestionBySlugUseCase } from '@/domain/forum/application/use-cases/get-question-by-slug'
 import { EditQuestionController } from './controllers/edit-question.controller'
 import { EditQuestionUseCase } from '@/domain/forum/application/use-cases/edit-question'
+import { DeleteQuestionController } from './controllers/delete-question.controller'
+import { DeleteQuestionUseCase } from '@/domain/forum/application/use-cases/delete-question'
 
 @Module({
   imports: [DatabaseModule, CryptographyModule],
@@ -23,6 +25,7 @@ import { EditQuestionUseCase } from '@/domain/forum/application/use-cases/edit-q
     FetchRecentQuestionsController,
     GetQuestionBySlugController,
     EditQuestionController,
+    DeleteQuestionController,
   ],
   // providers: controller utiliza UseCase na injeção de dependência
   providers: [
@@ -32,6 +35,7 @@ import { EditQuestionUseCase } from '@/domain/forum/application/use-cases/edit-q
     RegisterStudentUseCase,
     GetQuestionBySlugUseCase,
     EditQuestionUseCase,
+    DeleteQuestionUseCase,
   ],
 })
 export class HttpModule {}
