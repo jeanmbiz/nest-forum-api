52be1384f00dbb9f71f1ea6cb7719ee86040503b
jeanmbiz <jeanmbiz@hotmail.com>
2024-11-11 17:00:08 -0300

    refactor: add abstract class

--- src/domain/forum/application/repositories/question-comments-repository.ts
+++ src/domain/forum/application/repositories/question-comments-repository.ts
@@ -1,13 +1,14 @@
 import { PaginationParams } from '@/core/repositories/pagination-params'
 import { QuestionComment } from '../../enterprise/entities/question-comment'
 
-export interface QuestionCommentsRepository {
+export abstract class QuestionCommentsRepository {
   // método create que vai receber uma Anser(entidade) e vai retornar uma Promise de void
-  create(questionComment: QuestionComment): Promise<void>
-  findById(id: string): Promise<QuestionComment | null>
-  findManyByQuestionId(
+  abstract create(questionComment: QuestionComment): Promise<void>
+  abstract findById(id: string): Promise<QuestionComment | null>
+  abstract findManyByQuestionId(
     questionId: string,
     params: PaginationParams,
   ): Promise<QuestionComment[]>
-  delete(questionComment: QuestionComment): Promise<void>
+
+  abstract delete(questionComment: QuestionComment): Promise<void>
 }
