db8343ddbe6c272cbd40e1b6e993867189f508ac
jeanmbiz <jeanmbiz@hotmail.com>
2024-11-12 07:00:59 -0300

    test: add FetchQuestionAnswersController test

---
+++ src/infra/http/controllers/fetch-question-answers.controller.e2e-spec.ts
@@ -0,0 +1,79 @@
+import { AppModule } from '@/infra/app.module'
+import { Test } from '@nestjs/testing'
+import { INestApplication } from '@nestjs/common'
+import request from 'supertest'
+import { JwtService } from '@nestjs/jwt'
+import { StudentFactory } from 'test/factories/make-student'
+import { QuestionFactory } from 'test/factories/make-question'
+import { DatabaseModule } from '@/infra/database/database.module'
+import { AnswerFactory } from 'test/factories/make-answer'
+
+describe('Fetch Question Answers (E2E)', () => {
+  let app: INestApplication
+  let studentFactory: StudentFactory
+  let questionFactory: QuestionFactory
+  let answerFactory: AnswerFactory
+  let jwt: JwtService
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule, DatabaseModule],
+      providers: [StudentFactory, QuestionFactory, AnswerFactory],
+    }).compile()
+
+    app = moduleRef.createNestApplication()
+
+    // injeção de dependência
+    studentFactory = moduleRef.get(StudentFactory)
+    questionFactory = moduleRef.get(QuestionFactory)
+    answerFactory = moduleRef.get(AnswerFactory)
+    jwt = moduleRef.get(JwtService)
+
+    await app.init()
+  })
+
+  test('[GET] /questions/:questionId/answers', async () => {
+    // cria usuário utilizando a factory
+    const user = await studentFactory.makePrismaStudent()
+
+    // cria token do usuário passando user.id
+    const accessToken = jwt.sign({ sub: user.id.toString() })
+
+    // criar perguntar pela factoy
+    const question = await questionFactory.makePrismaQuestion({
+      authorId: user.id,
+      title: 'Question 01',
+    })
+
+    // cria respostas para a pergunta utilizando factory
+    await Promise.all([
+      answerFactory.makePrismaAnswer({
+        authorId: user.id,
+        questionId: question.id,
+        content: 'Answer 01',
+      }),
+      answerFactory.makePrismaAnswer({
+        authorId: user.id,
+        questionId: question.id,
+        content: 'Answer 02',
+      }),
+    ])
+
+    // pega id da pergunta
+    const questionId = question.id.toString()
+
+    // faz requisição para a rota
+    const response = await request(app.getHttpServer())
+      .get(`/questions/${questionId}/answers`)
+      .set('Authorization', `Bearer ${accessToken}`)
+      .send()
+
+    expect(response.statusCode).toBe(200)
+    expect(response.body).toEqual({
+      answers: expect.arrayContaining([
+        expect.objectContaining({ content: 'Answer 01' }),
+        expect.objectContaining({ content: 'Answer 02' }),
+      ]),
+    })
+  })
+})
