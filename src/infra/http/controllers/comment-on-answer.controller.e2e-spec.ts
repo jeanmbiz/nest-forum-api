a85a1d156f129dfa38b81f151c7caca218ab3021
jeanmbiz <jeanmbiz@hotmail.com>
2024-11-12 09:45:58 -0300

    test: add CommentOnAnswerController test

---
+++ src/infra/http/controllers/comment-on-answer.controller.e2e-spec.ts
@@ -0,0 +1,79 @@
+import { AppModule } from '@/infra/app.module'
+import { Test } from '@nestjs/testing'
+import { INestApplication } from '@nestjs/common'
+import request from 'supertest'
+import { JwtService } from '@nestjs/jwt'
+import { DatabaseModule } from '@/infra/database/database.module'
+import { StudentFactory } from 'test/factories/make-student'
+import { PrismaService } from '@/infra/database/prisma/prisma.service'
+import { AnswerFactory } from 'test/factories/make-answer'
+import { QuestionFactory } from 'test/factories/make-question'
+
+describe('Comment On Answer (E2E)', () => {
+  let app: INestApplication
+  let prisma: PrismaService
+  let studentFactory: StudentFactory
+  let questionFactory: QuestionFactory
+  let answerFactory: AnswerFactory
+  let jwt: JwtService
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule, DatabaseModule],
+      providers: [StudentFactory, QuestionFactory, AnswerFactory],
+    }).compile()
+
+    app = moduleRef.createNestApplication()
+
+    // injeção de dependência
+    studentFactory = moduleRef.get(StudentFactory)
+    questionFactory = moduleRef.get(QuestionFactory)
+    answerFactory = moduleRef.get(AnswerFactory)
+    prisma = moduleRef.get(PrismaService)
+    jwt = moduleRef.get(JwtService)
+
+    await app.init()
+  })
+
+  test('[POST] /answers/:answerId/comments', async () => {
+    // cria usuário utilizando a factory
+    const user = await studentFactory.makePrismaStudent()
+
+    // cria token do usuário passando user.id
+    const accessToken = jwt.sign({ sub: user.id.toString() })
+
+    // criar pergunta usando factory
+    const question = await questionFactory.makePrismaQuestion({
+      authorId: user.id,
+    })
+
+    // criar resposta da pergunta usando factory
+    const answer = await answerFactory.makePrismaAnswer({
+      questionId: question.id,
+      authorId: user.id,
+    })
+
+    // pego id da pergunta criada
+    const answerId = answer.id.toString()
+
+    // faz requisição para a rota
+    const response = await request(app.getHttpServer())
+      .post(`/answers/${answerId}/comments`)
+      .set('Authorization', `Bearer ${accessToken}`)
+      .send({
+        content: 'New comment',
+      })
+
+    // igual HttpCode do controller
+    expect(response.statusCode).toBe(201)
+
+    // busca comentário da resposta no banco de dados
+    const commentOnDatabase = await prisma.comment.findFirst({
+      where: {
+        content: 'New comment',
+      },
+    })
+
+    expect(commentOnDatabase).toBeTruthy()
+  })
+})
