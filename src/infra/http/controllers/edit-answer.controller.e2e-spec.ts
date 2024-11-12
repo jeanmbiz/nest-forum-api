b51dc92ff79ef21971a3b09efc23161887944187
jeanmbiz <jeanmbiz@hotmail.com>
2024-11-12 06:26:53 -0300

    test: add EditAnswerController test

---
+++ src/infra/http/controllers/edit-answer.controller.e2e-spec.ts
@@ -0,0 +1,79 @@
+import { AppModule } from '@/infra/app.module'
+import { Test } from '@nestjs/testing'
+import { INestApplication } from '@nestjs/common'
+import request from 'supertest'
+import { JwtService } from '@nestjs/jwt'
+import { DatabaseModule } from '@/infra/database/database.module'
+import { StudentFactory } from 'test/factories/make-student'
+import { PrismaService } from '@/infra/database/prisma/prisma.service'
+import { QuestionFactory } from 'test/factories/make-question'
+import { AnswerFactory } from 'test/factories/make-answer'
+
+describe('Edit Answer (E2E)', () => {
+  let app: INestApplication
+  let prisma: PrismaService
+  let studentFactory: StudentFactory
+  let answerFactory: AnswerFactory
+  let questionFactory: QuestionFactory
+  let jwt: JwtService
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule, DatabaseModule],
+      providers: [StudentFactory, QuestionFactory, AnswerFactory],
+    }).compile()
+
+    app = moduleRef.createNestApplication()
+
+    // injeção de dependência
+    studentFactory = moduleRef.get(StudentFactory)
+    questionFactory = moduleRef.get(QuestionFactory)
+    answerFactory = moduleRef.get(AnswerFactory)
+    prisma = moduleRef.get(PrismaService)
+    jwt = moduleRef.get(JwtService)
+
+    await app.init()
+  })
+
+  test('[PUT] /answers/:id', async () => {
+    // cria usuário utilizando a factory
+    const user = await studentFactory.makePrismaStudent()
+
+    // cria token do usuário passando user.id
+    const accessToken = jwt.sign({ sub: user.id.toString() })
+
+    // criar pergunta usando factory
+    const question = await questionFactory.makePrismaQuestion({
+      authorId: user.id,
+    })
+
+    // criar resposta da pertgunta usando factory
+    const answer = await answerFactory.makePrismaAnswer({
+      questionId: question.id,
+      authorId: user.id,
+    })
+
+    // pego id da pergunta criada
+    const answerId = answer.id.toString()
+
+    // faz requisição para a rota
+    const response = await request(app.getHttpServer())
+      .put(`/answers/${answerId}`)
+      .set('Authorization', `Bearer ${accessToken}`)
+      .send({
+        content: 'New answer content',
+      })
+
+    // igual HttpCode do controller
+    expect(response.statusCode).toBe(204)
+
+    // busca pergunta no banco de dados
+    const answerOnDatabase = await prisma.answer.findFirst({
+      where: {
+        content: 'New answer content',
+      },
+    })
+
+    expect(answerOnDatabase).toBeTruthy()
+  })
+})
