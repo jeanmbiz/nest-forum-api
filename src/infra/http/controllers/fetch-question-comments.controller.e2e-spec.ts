a643a1b8a5ed23d8df6c1c80e3d8e9b832fb5d51
jeanmbiz <jeanmbiz@hotmail.com>
2024-11-13 08:36:48 -0300

    test: add FetchQuestionCommentsController test

---
+++ src/infra/http/controllers/fetch-question-comments.controller.e2e-spec.ts
@@ -0,0 +1,79 @@
+import { AppModule } from '@/infra/app.module'
+import { Test } from '@nestjs/testing'
+import { INestApplication } from '@nestjs/common'
+import request from 'supertest'
+import { JwtService } from '@nestjs/jwt'
+import { StudentFactory } from 'test/factories/make-student'
+import { QuestionFactory } from 'test/factories/make-question'
+import { DatabaseModule } from '@/infra/database/database.module'
+import { QuestionCommentFactory } from 'test/factories/make-question-commet'
+
+describe('Fetch Question Comments (E2E)', () => {
+  let app: INestApplication
+  let studentFactory: StudentFactory
+  let questionFactory: QuestionFactory
+  let questionCommentFactory: QuestionCommentFactory
+  let jwt: JwtService
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule, DatabaseModule],
+      providers: [StudentFactory, QuestionFactory, QuestionCommentFactory],
+    }).compile()
+
+    app = moduleRef.createNestApplication()
+
+    // injeção de dependência
+    studentFactory = moduleRef.get(StudentFactory)
+    questionFactory = moduleRef.get(QuestionFactory)
+    questionCommentFactory = moduleRef.get(QuestionCommentFactory)
+    jwt = moduleRef.get(JwtService)
+
+    await app.init()
+  })
+
+  test('[GET] /questions/:questionId/comments', async () => {
+    // cria usuário utilizando a factory
+    const user = await studentFactory.makePrismaStudent()
+
+    // cria token do usuário passando user.id
+    const accessToken = jwt.sign({ sub: user.id.toString() })
+
+    // criar perguntar pela factoy
+    const question = await questionFactory.makePrismaQuestion({
+      authorId: user.id,
+      title: 'Question 01',
+    })
+
+    // cria comentários para a pergunta utilizando factory
+    await Promise.all([
+      questionCommentFactory.makePrismaQuestionComment({
+        authorId: user.id,
+        questionId: question.id,
+        content: 'Comment 01',
+      }),
+      questionCommentFactory.makePrismaQuestionComment({
+        authorId: user.id,
+        questionId: question.id,
+        content: 'Comment 02',
+      }),
+    ])
+
+    // pega id da pergunta
+    const questionId = question.id.toString()
+
+    // faz requisição para a rota
+    const response = await request(app.getHttpServer())
+      .get(`/questions/${questionId}/comments`)
+      .set('Authorization', `Bearer ${accessToken}`)
+      .send()
+
+    expect(response.statusCode).toBe(200)
+    expect(response.body).toEqual({
+      comments: expect.arrayContaining([
+        expect.objectContaining({ content: 'Comment 01' }),
+        expect.objectContaining({ content: 'Comment 02' }),
+      ]),
+    })
+  })
+})
