331e005c34bf448e415a18899d2d1c8ad99ba7a7
jeanmbiz <jeanmbiz@hotmail.com>
2024-11-20 10:22:57 -0300

    feat: add FakeUploader

---
+++ test/storage/fake-uploader.ts
@@ -0,0 +1,26 @@
+import {
+  UploadParams,
+  Uploader,
+} from '@/domain/forum/application/storage/uploader'
+import { randomUUID } from 'crypto'
+
+interface Upload {
+  fileName: string
+  url: string
+}
+
+export class FakeUploader implements Uploader {
+  // como se fosse uma pasta, guardando os arquivos
+  public uploads: Upload[] = []
+
+  async upload({ fileName }: UploadParams): Promise<{ url: string }> {
+    const url = randomUUID()
+
+    this.uploads.push({
+      fileName,
+      url,
+    })
+
+    return { url }
+  }
+}
