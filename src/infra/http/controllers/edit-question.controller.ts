95c6ea182c4e4f4c87026dcf3f9f9b6b2d1f9ff2
jeanmbiz <jeanmbiz@hotmail.com>
2024-11-23 15:50:43 -0300

    featr: add attachments

--- src/infra/http/controllers/edit-question.controller.ts
+++ src/infra/http/controllers/edit-question.controller.ts
@@ -16,6 +16,7 @@ import { EditQuestionUseCase } from '@/domain/forum/application/use-cases/edit-q
 const editQuestionBodySchema = z.object({
   title: z.string(),
   content: z.string(),
+  attachments: z.array(z.string().uuid()),
 })
 
 // Criar tipagem para o Schema zod
@@ -41,14 +42,14 @@ export class EditQuestionController {
     // id é parametro, sendo renomeado para questionId
     @Param('id') questionId: string,
   ) {
-    const { title, content } = body
+    const { title, content, attachments } = body
     const userId = user.sub
 
     const result = await this.editQuestion.execute({
       title,
       content,
       authorId: userId,
-      attachmentsIds: [],
+      attachmentsIds: attachments,
       questionId,
     })
 
