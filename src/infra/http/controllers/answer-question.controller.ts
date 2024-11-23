5098e58b1ecb1cff52bcfc46a1fcd3df4e183371
jeanmbiz <jeanmbiz@hotmail.com>
2024-11-23 15:54:07 -0300

    feat: add attachments

--- src/infra/http/controllers/answer-question.controller.ts
+++ src/infra/http/controllers/answer-question.controller.ts
@@ -14,6 +14,7 @@ import { AnswerQuestionUseCase } from '@/domain/forum/application/use-cases/answ
 // cria Schema de validação com zod
 const answerQuestionBodySchema = z.object({
   content: z.string(),
+  attachments: z.array(z.string().uuid()),
 })
 
 // Criar tipagem para o Schema zod
@@ -37,14 +38,14 @@ export class AnswerQuestionController {
     @CurrentUser() user: UserPayload,
     @Param('questionId') questionId: string,
   ) {
-    const { content } = body
+    const { content, attachments } = body
     const userId = user.sub
 
     const result = await this.answerQuestion.execute({
       content,
       questionId,
       authorId: userId,
-      attachmentsIds: [],
+      attachmentsIds: attachments,
     })
 
     if (result.isLeft()) {
